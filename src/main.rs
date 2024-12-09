181e0092785272944f681b099c8d0c7afd309717
Matthew Smith <mattsmithhimself@gmail.com>
2024-12-08 20:15:32 -0500

    experimental christmas features

--- src/main.rs
+++ src/main.rs
@@ -28,6 +28,7 @@ struct Outlet {
     ip_addr: Option<String>,
     alias: Option<String>,
     idx: Option<usize>,
+    on_off: Option<String>,
 }
 
 #[derive(Clone)]
@@ -60,11 +61,59 @@ async fn main() {
         .route("/discover", get(discover_devices))
         //.route("/plugs", get(get_plugs))
         .route("/toggle", post(toggle_outlet))
+        .route("/dance", post(dance))
+        .route("/tree", post(tree))
+        .route("/christmas", post(christmas))
         .with_state(state);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:4000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
+
+async fn dance( 
+    State(state): State<Arc<Mutex<DeviceState>>>,
+    Json(payload): Json<Outlet>) -> StatusCode {
+    
+    return StatusCode::OK;
+}
+
+async fn tree( 
+    State(state): State<Arc<Mutex<DeviceState>>>,
+    Json(payload): Json<Outlet>) -> StatusCode {
+    
+    return StatusCode::OK;
+}
+
+async fn christmas( 
+    State(state): State<Arc<Mutex<DeviceState>>>,
+    Json(payload): Json<Outlet>) -> StatusCode {
+    
+
+    if let Ok(mut this_state) = state.lock() {
+
+        if let Some(on_off) = payload.on_off {
+        let lights = find_dev_with_alias(&mut this_state, &"lights".to_string());
+        let tree = find_dev_with_alias(&mut this_state, &"ChristmasTree".to_string());
+        let set_to = match on_off.as_str() {
+            "on" => 1,
+            "off" => 0,
+            _ => 0,
+        };
+
+        if let (Some(lights), Some(tree)) = (lights, tree) {
+            lights.set_single_relay(set_to);
+            println!("are we getting here?");
+            for i in 0..5 {
+                tree.set_child_relay_by_idx(i as usize, set_to);
+            }
+        }
+        }
+    } else {
+        println!("couldn't get state");
+    }
+    return StatusCode::OK;
+}
+
 
 async fn discover_devices(
 ) -> (StatusCode, Json<Vec<models::SysInfo>>) {
@@ -129,11 +178,12 @@ async fn toggle_outlet(
     Json(payload): Json<Outlet>) -> StatusCode {
 
     let mut target: Option<Device> = None;
-    
+    println!("in toggle_outlet"); 
     if let Ok(mut this_state) = state.lock() {
         //user set alias field, see if it exists
         if let Some(alias) = payload.alias {
             if alias_exists_with_refresh(&mut this_state, &alias) {
+                println!("alias exists");
                 target = find_dev_with_alias(&mut this_state,  &alias);
             }
         }
@@ -153,11 +203,13 @@ async fn toggle_outlet(
             //we have a target
             // now, did user provide an idx? if so, does the device support idx'd outlets?
             if t.has_children() {
+                println!("thinks it has children");
                 if let Some(idx) = payload.idx {
                     t.toggle_relay_by_id(idx);
                     return StatusCode::OK;
                 }
             } else {
+                println!("toggle single");
                 t.toggle_single_relay();
                 return StatusCode::OK;
             }
